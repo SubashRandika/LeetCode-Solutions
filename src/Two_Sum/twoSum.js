6dc766265210075070f92205fe8be8c412824b6f
Randika Rodrigo <subash.rodrigo@gmail.com>
2021-06-16 23:15:07 +0530

    Updated Question number to comment.

--- src/Two_Sum/twoSum.js
+++ src/Two_Sum/twoSum.js
@@ -1,5 +1,5 @@
 /**
- * Given an array of integers numbers and an integer target,
+ * 1. Given an array of integers numbers and an integer target,
  * return indices of the two numbers such that they add up to target.
  *
  * Time Complexity: O(n)
